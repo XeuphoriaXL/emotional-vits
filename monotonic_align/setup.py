53583ebc4d80184ad075c560669d5ee2723d263e
Peter Luo <98758402+XeuphoriaXL@users.noreply.github.com>
2024-10-29 20:21:30 -0400

    Update setup.py

--- monotonic_align/setup.py
+++ monotonic_align/setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy
-import os
-
-output_dir = os.path.join(os.getcwd(), 'monotonic_align')
 
 setup(
     name='monotonic_align',
     ext_modules=cythonize("core.pyx", language_level="3"),
     include_dirs=[numpy.get_include()],
-    script_args=["build_ext", "--build-lib", output_dir]
 )
